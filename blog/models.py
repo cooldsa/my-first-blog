3a417e37122b5c1bc095f86dac6b24a74595a89c
admin <admin@admin.com>
2016-03-10 17:56:41 +0500

    Small fixes

--- blog/models.py
+++ blog/models.py
@@ -7,11 +7,11 @@ class Post(models.Model):
 	text = models.TextField()
 	created_date = models.DateTimeField(
 			default = timezone.now)
-	publihed_date = models.DateTimeField(
+	published_date = models.DateTimeField(
 			blank=True, null = True)
 	
 	def publish(self):
-		self.publihed_date = timezone.now()
+		self.published_date = timezone.now()
 		self.save()
 	
 	def __str__(self):
